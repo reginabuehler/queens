e6453bfa86da7668a29a00192c21e379768ddd97
reginabuehler <a11brebu@login.cluster.internal>
2025-07-14 17:48:59 +0200

    test: add test of inverse_cdf_transform for UniformDiscrete parametere

--- tests/unit_tests/parameters/test_parameters.py
+++ tests/unit_tests/parameters/test_parameters.py
@@ -19,6 +19,7 @@ import pytest
 
 from queens.distributions.normal import Normal
 from queens.distributions.uniform import Uniform
+from queens.distributions.uniform_discrete import UniformDiscrete
 from queens.parameters.parameters import Parameters, from_config_create_parameters
 
 
@@ -35,6 +36,14 @@ def fixture_parameters_set_2():
     """Parameters dict without random field."""
     x1 = Uniform(lower_bound=-5, upper_bound=10)
     x2 = Normal(mean=0, covariance=1)
+    return Parameters(x1=x1, x2=x2)
+
+
+@pytest.fixture(name="parameters_set_3", scope="module")
+def fixture_parameters_set_3():
+    """Parameters dict without random field."""
+    x1 = Uniform(lower_bound=-5, upper_bound=10)
+    x2 = UniformDiscrete(sample_space=[[0], [1], [2], [3], [4]])
     return Parameters(x1=x1, x2=x2)
 
 
@@ -89,7 +98,7 @@ def test_joint_logpdf(parameters_set_1):
     np.testing.assert_almost_equal(logpdf, np.array([-np.inf, -15.14250]), decimal=5)
 
 
-def test_inverse_cdf_transform(parameters_set_1, parameters_set_2):
+def test_inverse_cdf_transform(parameters_set_1, parameters_set_2, parameters_set_3):
     """Test *inverse_cdf_transform* method."""
     samples = np.array([0.5, 0.1, 0.6])
     with pytest.raises(ValueError):
@@ -104,6 +113,10 @@ def test_inverse_cdf_transform(parameters_set_1, parameters_set_2):
     np.testing.assert_almost_equal(
         transformed_samples, np.array([[2.50000, -1.28155], [10.00000, -1.28155]]), decimal=5
     )
+
+    samples = np.array([[0.5, 0.1], [0.5, 0.6]])
+    transformed_samples = parameters_set_3.inverse_cdf_transform(samples)
+    np.testing.assert_almost_equal(transformed_samples, np.array([[2.5, 0], [2.5, 2]]), decimal=5)
 
 
 def test_sample_as_dict(parameters_set_1):
